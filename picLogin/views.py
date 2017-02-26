605965eeebf05c314c9155c0a6c2dcdbefb0134b
David Lopez <lopezda314@gmail.com>
2017-02-26 16:20:55 -0600

    Adding to github

---
+++ picLogin/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    output = 'Hello World - home'
+    return HttpResponse(output)
+
+
+def profile(request):
+    output = 'Hello World! Configured for usernames using request context processing. My username is: ' + request.user.username 
+    return HttpResponse(output)
